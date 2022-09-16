d6fad56ae5ac6b7cf1c25b404ff27106dad9cccc
tanya1081917 <tanya.jain1917@gmail.com>
2022-09-16 10:59:01 +0530

    commit for service

---
+++ src/test/java/com/energy/zeal/ZealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energy.zeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
